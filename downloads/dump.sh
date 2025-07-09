2a200e459043199a210988e7dc6d9adbdac88375
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-07-09 16:01:42 -0700

    add NCIT dump source

--- downloads/dump.sh
+++ downloads/dump.sh
@@ -6,6 +6,7 @@ urls=(
   "http://gepa.org.cn/static2/file/disease_species.csv"
   "http://gepa.org.cn/static2/file/meta_gene_net.csv"
   "http://bigg.ucsd.edu/static/namespace/bigg_models_metabolites.txt"
+  "https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/Thesaurus_25.06e.FLAT.zip"
 )
 
 for url in "${urls[@]}"; do
